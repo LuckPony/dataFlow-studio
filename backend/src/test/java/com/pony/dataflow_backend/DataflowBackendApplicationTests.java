bf8fe3319767351a19f55942d7fc6032701d03c8
Suchao Ma <masuchao2023@163.com>
2026-07-17 15:47:10 +0800

    init backend

---
+++ backend/src/test/java/com/pony/dataflow_backend/DataflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pony.dataflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
